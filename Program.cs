99738f65aa5ab3a0192dfa00750a68b19cabddf8
TaraHsani <147997426+TaraHsani@users.noreply.github.com>
2023-10-31 13:17:49 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Metod
+{
+    internal class Program
+    {
+
+        public static int Add(int firstNumber, int secondNumber, int thirdNumber)
+        {
+
+            return firstNumber * secondNumber * thirdNumber;
+
+        }
+        static void Main(string[] args)
+        {
+
+            int result = Add(5, 4, 2);
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
